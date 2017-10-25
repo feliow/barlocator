15612d284a2ad4259d958b30f72ac79f03b8cc23
feliow <32958820+feliow@users.noreply.github.com>
2017-10-25 09:11:30 +0200

    Testing

--- contact.php
+++ contact.php
@@ -27,6 +27,9 @@
 			<P>ELLET 073-5986774</P>
 			<P>stinisen 073-5986774</P>
 			<P>elin 073-5986774</P>
+			<P>ELLET 073-5986774</P>
+			<P>stinisen 073-5986774</P>
+			<P>elin 073-5986774</P>
 			<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent rutrum quis arcu in molestie. 
 				Nulla vehicula a justo a vestibulum. </p>
 
