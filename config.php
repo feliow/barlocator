0ef84a215401decc1dd1ec7b59f1084a83674284
stinabjornberg <33080283+stinabjornberg@users.noreply.github.com>
2017-10-25 11:25:28 +0200

    lagt till sql injections

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php 
+//$current_page = end(explode('/', $_SERVER['REQUEST_URI']));
+$dbname = 'barlocator';
+$dbuser = 'root';
+$dbpass = 'root';
+$dbserver = 'localhost';
+?>
--- contact.php
+++ contact.php
@@ -1,41 +1,31 @@
-<DOCTYPE! html>
-<html>
-	<head>
-		<title>Barlocator</title>
-		<link rel="stylesheet" type="text/css" href="css/barlocator.css">
-		<meta charset="utf-8">
-	</head>
-<header>
-	<div class="menu">
-	<a href="">BARS</a>
-	<a href="">FAVORITES</a>
-	<img src="img/logo.png">
-	<a href="about.php">ABOUT US</a>
-	<a href="contact.php">CONTACTS</a>
-	</div>
-</header>
+<?include ("header.php");?>s
 <body>
-
-
 	<div id="pagecontainer">
-		
 		<div class="contact">
+			<?php
+		                echo "Upload a picture: ";
+		                echo '<a class="return" href="SQLInjection.php">click here</a>';
+		                echo '<br/>';
+		     $files = glob("uploadedfiles/*.*");
+		     for ($i=0; $i<count($files); $i++)
+		      {
+		        $image = $files[$i];
+		        $supported_file = array(
+		                'gif',
+		                'jpg',
+		                'jpeg',
+		                'png'
+		         );
 
-			<img src="img/aboutpic.jpg"/>
-			<h2>CONTACT</h2>
-
-			<P>ELLET 073-5986774</P>
-			<P>stinisen 073-5986774</P>
-			<P>elin 073-5986774</P>
-			
-			<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent rutrum quis arcu in molestie. 
-				Nulla vehicula a justo a vestibulum. </p>
-
-
+		         $ext = strtolower(pathinfo($image, PATHINFO_EXTENSION));
+		         if (in_array($ext, $supported_file)) {
+		             echo '<img class="upimg" src="'.$image .'" alt="Random image" width="200px" />';
+		            } else {
+		                continue;
+		            }
+		          }
+		       ?>
 		</div>
-
-
-
 	</div>
 </body>
 </html>
