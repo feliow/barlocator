3f8d0bbd055d5b59ec01cd3137cfc2b1881bad29
feliow <32958820+feliow@users.noreply.github.com>
2017-10-25 06:49:00 +0200

    First Try

---
+++ index.php
@@ -0,0 +1,22 @@
+<DOCTYPE! html>
+<html>
+	<head>
+		<title>Barlocator</title>
+		<link rel="stylesheet" type="text/css" href="css/barlocator.css">
+		<meta charset="utf-8">
+	</head>
+<header>
+	<div class="menu">
+	<a href="">BARS</a>
+	<a href="">FAVORITES</a>
+	<img src="img/logo.png">
+	<a href="">ABOUT US</a>
+	<a href="">CONTACTS</a>
+	</div>
+</header>
+<body>
+	<div id="pagecontainer">
+		<input type="text" name="search" placeholder="search for ya bar!!!!">
+	</div>
+</body>
+</html>
