82c8259e3cf101a50d363ae098f7afc7519ef7a4
stinabjornberg <33080283+stinabjornberg@users.noreply.github.com>
2017-10-25 10:20:01 +0200

    ändrade till hej

--- about.php
+++ about.php
@@ -20,7 +20,7 @@
 		<div class="about">
 
 			<img src="img/aboutpic.jpg"/>
-			<h2>SWAGGER LIKE OBAMA</h2>
+			<h2>HEj</h2>
 			<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent rutrum quis arcu in molestie. 
 				Nulla vehicula a justo a vestibulum. Duis luctus elit vel felis congue porttitor.
 				 Ut vel est sit amet odio tincidunt convallis in ut risus. Praesent suscipit odio a egestas suscipit. 
