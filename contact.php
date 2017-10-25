785feb8ba8a58b6afb28252ed6c3e709c78a2ece
feliow <32958820+feliow@users.noreply.github.com>
2017-10-25 09:26:15 +0200

    changes of colour

--- contact.php
+++ contact.php
@@ -27,9 +27,7 @@
 			<P>ELLET 073-5986774</P>
 			<P>stinisen 073-5986774</P>
 			<P>elin 073-5986774</P>
-			<P>ELLET 073-5986774</P>
-			<P>stinisen 073-5986774</P>
-			<P>elin 073-5986774</P>
+			
 			<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent rutrum quis arcu in molestie. 
 				Nulla vehicula a justo a vestibulum. </p>
 
