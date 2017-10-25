7b9532ac9a793d072f247a8518674a7edb035623
feliow <32958820+feliow@users.noreply.github.com>
2017-10-25 11:20:04 +0200

    adding

--- about.php
+++ about.php
@@ -1,39 +1,15 @@
-<DOCTYPE! html>
-<html>
-	<head>
-		<title>Barlocator</title>
-		<link rel="stylesheet" type="text/css" href="css/barlocator.css">
-		<meta charset="utf-8">
-	</head>
-<header>
-	<div class="menu">
-	<a href="">BARS</a>
-	<a href="">FAVORITES</a>
-	<img src="img/logo.png">
-	<a href="about.php">ABOUT US</a>
-	<a href="contact.php">CONTACTS</a>
-	</div>
-</header>
+<?include ("header.php");?>
 <body>
 	<div id="pagecontainer">
-		
 		<div class="about">
-
 			<img src="img/aboutpic.jpg"/>
-			<h2>HEj</h2>
+			<h2>SWAGGER LIKE OBAMA</h2>
 			<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent rutrum quis arcu in molestie. 
 				Nulla vehicula a justo a vestibulum. Duis luctus elit vel felis congue porttitor.
 				 Ut vel est sit amet odio tincidunt convallis in ut risus. Praesent suscipit odio a egestas suscipit. 
 				 Pellentesque convallis elit sed erat bibendum, quis faucibus leo ullamcorper. 
 				 Pellentesque maximus in lorem nec vestibulum. </p>
-
-
-
 		</div>
-
 	</div>
-
-
-
 </body>
 </html>
--- contact.php
+++ contact.php
@@ -1,41 +1,15 @@
-<DOCTYPE! html>
-<html>
-	<head>
-		<title>Barlocator</title>
-		<link rel="stylesheet" type="text/css" href="css/barlocator.css">
-		<meta charset="utf-8">
-	</head>
-<header>
-	<div class="menu">
-	<a href="">BARS</a>
-	<a href="">FAVORITES</a>
-	<img src="img/logo.png">
-	<a href="about.php">ABOUT US</a>
-	<a href="contact.php">CONTACTS</a>
-	</div>
-</header>
+<?include ("header.php");?>s
 <body>
-
-
 	<div id="pagecontainer">
-		
 		<div class="contact">
-
 			<img src="img/aboutpic.jpg"/>
 			<h2>CONTACT</h2>
-
 			<P>ELLET 073-5986774</P>
 			<P>stinisen 073-5986774</P>
 			<P>elin 073-5986774</P>
-			
 			<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent rutrum quis arcu in molestie. 
 				Nulla vehicula a justo a vestibulum. </p>
-
-
 		</div>
-
-
-
 	</div>
 </body>
 </html>
